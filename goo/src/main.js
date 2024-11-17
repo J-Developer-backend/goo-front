4f6d44baaec3b47d85e70ac9e5a29d11a21c7595
J <J.@qq.com>
2024-11-17 19:56:39 +0800

    feat: 添加请求拦截器

--- goo/src/main.js
+++ goo/src/main.js
@@ -4,6 +4,25 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+// 设置请求拦截器
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      config.headers['token'] = `${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 将axios挂载到Vue的原型上，使其在组件中可用
+Vue.prototype.$http = axios;
+
 
 Vue.config.productionTip = false
 
